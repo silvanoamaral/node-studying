1cf54e9aef18f70ff069c4f88b4c5df54e1f90de
Silvano Amaral <silvano.amaral9@gmail.com>
2018-06-26 18:18:32 -0300

    Primeiro arquivo

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ var app = require('./config/custom-express')();
 
 //listen define que esta ouvindo uma determinada porta
 app.listen(5000, function() {
-    console.log('Servidor rodando na porta 5000');
+    console.log('Servidor rodando na porta 5000.');
 });
 
